c0698242776b1141abc13e7c641a173cd1068702
gonza <gonza.andre2@gmail.com>
2019-10-17 23:12:29 -0300

    probando conexion

---
+++ PatrimonioInmobiliario-respaldo/Auth_users/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+from django.contrib.auth import authenticate, logout, login
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
+
+def auth_login(request):
+    template_name = 'login.html'
+    data = {}
+    logout(request)
+    username = password = ''
+
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(
+            username=username,
+            password=password
+        )
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+            else:
+                print("usuario o contraseña no validos")
+                messages.warning(
+                    request,
+                    'Usuario o contraseña incorrectos!'
+                )
+        else:
+            print("Usuario incorrecto")
+            messages.error(
+                request,
+                'Usuario o contraseña incorrectos!'
+            )
+    return render(request, template_name, data)
+
+def auth_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('login'))
---
+++ PatrimonioInmobiliario-respaldo/propiedades2/admin.py
@@ -0,0 +1,32 @@
+from django.contrib import admin
+from propiedades2.models import Acquisition, DocumentEx, DocumentCip, DocumentCn, DocumentBlue, DocumentBuildP, \
+    DocumentMR, DocumentTypeC, DocumentOther, DocumentWR, DocumentDC, DocumentPH, DocumentDB, DocumentAc, DocumentEs, \
+    Location,ArchitectureRecordAcq, InternalAccountantsAcq, NotaryAcquisition, SiiRecord, Rent, Post, Staff, Region, Comment, Property
+# Register your models here.
+
+admin.site.register(Location)
+admin.site.register(Acquisition)
+admin.site.register(ArchitectureRecordAcq)
+admin.site.register(InternalAccountantsAcq)
+admin.site.register(NotaryAcquisition)
+admin.site.register(SiiRecord)
+admin.site.register(DocumentEx)
+admin.site.register(DocumentCip)
+admin.site.register(DocumentCn)
+admin.site.register(DocumentBlue)
+admin.site.register(DocumentBuildP)
+admin.site.register(DocumentMR)
+admin.site.register(DocumentTypeC)
+admin.site.register(DocumentOther)
+admin.site.register(DocumentWR)
+admin.site.register(DocumentDC)
+admin.site.register(DocumentPH)
+admin.site.register(DocumentDB)
+admin.site.register(DocumentAc)
+admin.site.register(DocumentEs)
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Rent)
+admin.site.register(Staff)
+admin.site.register(Region)
+admin.site.register(Property)
